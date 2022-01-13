d335addb90ca560d77ffbd4b4de137fed78de6b8
marquescami <camilamsc@hotmail.com>
2022-01-13 20:01:44 -0300

    Initial Commit

---
+++ Movie-swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movie-swift
+//
+//  Created by Camila Marques Vasconcelos Loureiro on 13/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Movie-swift/Movie_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_swiftApp.swift
+//  Movie-swift
+//
+//  Created by Camila Marques Vasconcelos Loureiro on 13/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Movie_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
